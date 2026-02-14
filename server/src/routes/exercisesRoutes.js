de0add288d54e7132822c26776370e725c165f6d
gpego1 <gpego786@gmail.com>
2026-02-14 18:49:40 -0300

    authorization up

--- server/src/routes/exercisesRoutes.js
+++ server/src/routes/exercisesRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { ExerciseController } from "../controllers/ExerciseController.js";
+import auth from "../middlewares/authMiddleware.js"
+import paginate from "../middlewares/paginate.js"
 
 const router = express.Router();
 
 router  
-    .get("/:id", ExerciseController.getExerciseById)
-    .get("", ExerciseController.getExerciseByMuscleTargetOrName)
+    .get("/:id", ExerciseController.getExerciseById, auth)
+    .get("", ExerciseController.getExerciseByMuscleTargetOrName, auth, paginate)
     .post("", ExerciseController.createExercise)
     .put("/:id", ExerciseController.updateExercises)
     .delete("/:id", ExerciseController.deleteExercises)
