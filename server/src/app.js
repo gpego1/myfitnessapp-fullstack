42d2aaf96e1960f0222bf8c2533c58aff12a3520
gpego1 <gpego786@gmail.com>
2026-02-15 01:23:41 -0300

    update server

--- server/src/app.js
+++ server/src/app.js
@@ -3,18 +3,22 @@ import db from "./config/dbConnect.js";
 import routes from "./routes/index.js"
 import errorHanlder from "./middlewares/ErrorHanlder.js";
 import notFoundEntityMiddleware from "./middlewares/NotFoundEntityHandler.js";
+import corsMiddleware from "./middlewares/corsMiddleware.js";
 
 db.on("error", console.log.bind(console, 'Error to connect with db'))
 db.once("open", () => {
     console.log("Succesfully conected with database")
 });
 
+
 const app = express();
+app.use(corsMiddleware);
 app.use(express.json());
 routes(app);
 
 app.use(errorHanlder);
 app.use(notFoundEntityMiddleware)
+
 
 
 
---
+++ server/src/middlewares/corsMiddleware.js
@@ -0,0 +1,13 @@
+import cors from "cors";
+
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5173/auth/login'];
+
+const corsOptions = {
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+const corsMiddleware = cors(corsOptions);
+
+export default corsMiddleware;
